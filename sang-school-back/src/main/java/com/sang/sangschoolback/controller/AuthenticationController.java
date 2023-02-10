c6abc1470b8176d4c2ba72be5990a63b96274ca4
sangareadama <sangadam7407@gmail.com>
2023-02-10 16:26:19 +0000

    recuperation

--- sang-school-back/src/main/java/com/sang/sangschoolback/controller/AuthenticationController.java
+++ sang-school-back/src/main/java/com/sang/sangschoolback/controller/AuthenticationController.java
@@ -1,12 +1,13 @@
 package com.sang.sangschoolback.controller;
 
+import com.sang.sangschoolback.domain.Utilisateur;
 import com.sang.sangschoolback.service.AuthenticationService;
+import com.sang.sangschoolback.service.IUtilisateurService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -14,6 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthenticationController {
 
     private final AuthenticationService authenticationService;
+
+    private final IUtilisateurService iUtilisateurService;
     @PostMapping("/register")
     public ResponseEntity<AuthenticationResponse> register(
             @RequestBody RegisterRequest request
@@ -26,5 +29,11 @@ public class AuthenticationController {
             @RequestBody AuthenticationRequest request
     ){
         return ResponseEntity.ok(authenticationService.authenticate(request));
+    }
+
+    @GetMapping("/liste")
+    public ResponseEntity<List<Utilisateur>> listeUtilisateure(){
+
+        return ResponseEntity.ok(iUtilisateurService.listeUtilisateur());
     }
 }
