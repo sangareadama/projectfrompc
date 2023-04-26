a658273c1e865cf71f3274d2dcbbc5624e11e3ef
sangareadama <sangadam7407@gmail.com>
2023-04-26 07:24:45 +0000

    commit flyway

--- sang-school-back/src/main/java/com/sang/sangschoolback/entity/Utilisateur.java
+++ sang-school-back/src/main/java/com/sang/sangschoolback/entity/Utilisateur.java
@@ -10,12 +10,20 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.util.Collection;
 import java.util.List;
 
+import static com.sang.sangschoolback.entity.Utilisateur.TABLE_NAME;
 import static org.apache.logging.log4j.util.Strings.isBlank;
 
 @Entity
+@Table(name = TABLE_NAME )
 public class Utilisateur implements UserDetails {
+
+    public static final String TABLE_NAME = "utilisateur";
+    public static final String TABLE_ID = TABLE_NAME + "_id";
+    public static final String TABLE_SEQ = TABLE_ID + "_seq";
+
     @Id
-    @GeneratedValue
+    @SequenceGenerator(name = TABLE_SEQ, sequenceName = TABLE_SEQ)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = TABLE_SEQ)
     private Long id;
     @Column(name = "nom")
     private String nom;
